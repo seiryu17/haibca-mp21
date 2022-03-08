53c2879a68d50855941419fdc74efc642aebd92e
Rio Riferro <gulivverio@gmail.com>
2022-03-08 19:50:17 +0700

    implement detail product page

--- src/main/java/com/example/demo/controller/MarketController.java
+++ src/main/java/com/example/demo/controller/MarketController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MarketController {
@@ -10,6 +11,13 @@ public class MarketController {
     @GetMapping("/market")
     public String market() {
         return "market/index";
+    }
+
+    // display list of employees
+    @GetMapping("/detail-product/{id}")
+
+    public String viewDetailProduct(@PathVariable(value = "id") long id) {
+        return "market/detail-product";
     }
 
 }
