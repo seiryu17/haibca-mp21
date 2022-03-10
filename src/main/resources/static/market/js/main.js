c0b9b764a3ecb668dc4850f679554ac44fa895d0
Rio Riferro <gulivverio@gmail.com>
2022-03-10 09:17:56 +0700

    implement item list in checkout page

--- src/main/resources/static/market/js/main.js
+++ src/main/resources/static/market/js/main.js
@@ -1,7 +1,3 @@
-const removeItem = (id) => {
-  console.log("pelin", id);
-};
-
 const show = () => {
   if (JSON.parse(localStorage.getItem("itemCart"))) {
     let tab = JSON.parse(localStorage.getItem("itemCart")).map((x) => {
@@ -15,6 +11,11 @@ const show = () => {
               <h6>${x.name}</h6>
             </div>
           </td>
+          <td class="delete-item">
+          <a id="removeItem" onclick="removeItem(${x.id})">
+            <i class="material-icons">close</i>
+          </a>
+        </td>
         </tr>`;
     });
     let total = 0;
@@ -29,6 +30,65 @@ const show = () => {
 };
 
 show();
+
+const showCheckout = () => {
+  if (JSON.parse(localStorage.getItem("itemCart"))) {
+    let tab = JSON.parse(localStorage.getItem("itemCart")).map((x) => {
+      return `
+        <tr>
+          <td class="cart-pic first-row">
+            <img src="/market/img/cart-page/product-1.jpg" />
+          </td>
+          <td class="cart-title first-row text-center">
+            <h5>${x.name}</h5>
+          </td>
+          <td class="p-price first-row">$${x.price}</td>
+          <td class="p-price first-row">${x.qty}</td>
+          <td class="delete-item">
+            <a id="removeItem" onclick="removeItem(${x.id})">
+              <i class="material-icons">close</i>
+            </a>
+          </td>
+        </tr>
+     `;
+    });
+    let total = 0;
+    JSON.parse(localStorage.getItem("itemCart")).map((x) => {
+      return (total = total + x.price * x.qty);
+    });
+    $(document).ready(function () {
+      document.getElementById("checkout-item-page").innerHTML = tab;
+      document.getElementById("total-purchase").innerHTML = `$` + total;
+    });
+  }
+};
+
+showCheckout();
+
+const removeItem = (id) => {
+  if (JSON.parse(localStorage.getItem("itemCart"))) {
+    let data = JSON.parse(localStorage.getItem("itemCart")).filter(
+      (x) => x.id !== id.toString()
+    );
+    localStorage.setItem("itemCart", JSON.stringify(data));
+  }
+  showCheckout();
+  show();
+};
+
+// removeItem.onclick = function () {
+//   console.log("asd");
+// };
+
+// $(document).ready(function () {
+//   $("#removeItem").click(function () {
+//     if (JSON.parse(localStorage.getItem("itemCart"))) {
+//       let data = JSON.parse(localStorage.getItem("itemCart")).filter((x) => x.id !== $(this).attr("data-id"));
+
+//     }
+//     console.log($(this).attr("data-id"));
+//   });
+// });
 
 const addToCart = (id, price, name) => {
   let temp = [];
